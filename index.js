d1283216aa327d0e11dbe4e593fbc6632b8c8959
Brian Omondi <brianhilsden@gmail.com>
2024-04-03 19:25:55 +0300

    posted data

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 function submitData(name, email) {
-    return(
-        fetch("http://localhost:3000/users", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-              "Accept": "application/json",
-            },
-            body: JSON.stringify({ name: name, email: email }),
-          })
-            .then(res => res.json())
-            .then(data => document.body.append(data.id))
-            .catch((error)=>{
-                document.body.append(error.message)
-            })
-    )
-
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({ name: name, email: email }),
+  })
+    .then((res) => res.json())
+    .then((data) => document.body.append(data.id))
+    .catch((error) => {
+      document.body.append(error.message);
+    });
 }
